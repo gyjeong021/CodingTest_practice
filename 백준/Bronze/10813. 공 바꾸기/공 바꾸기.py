ee4b12e7fe7e993942006a5ea598148a8be60557
Jiyoung <gyjeong021@gmail.com>
2025-06-10 23:50:41 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,16 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+n_box = []
+
+for i in range(n) :
+    n_box.append(i+1)
+
+for i in range(m) :
+    i_box, j_box = map(int, sys.stdin.readline().split())
+    x = n_box[i_box-1]
+    n_box[i_box-1] = n_box[j_box-1]
+    n_box[j_box-1] = x
+
+for i in n_box :
+    print(i, end=" ")
