6986ff104270c6821fe6b76db67e5dfca0ab1589
Jiyoung <gyjeong021@gmail.com>
2025-06-11 11:43:11 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,15 +1,10 @@
 import sys
 
-list = []
-s_list = []
+list = [i for i in range(1,31)]
 
 for i in range(28) :
     num = int(sys.stdin.readline())
-    list.append(num)
+    list.remove(num)
 
-for i in range(1,31) :
-    if i not in list :
-        s_list.append(i)
-
-print(min(s_list))
-print(max(s_list))
+print(min(list))
+print(max(list))
